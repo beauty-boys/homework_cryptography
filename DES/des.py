589e2fb432cb5a409d59e5f00afecb15a0571635
beauty-boys <2369976351@qq.com>
2024-01-03 21:12:52 +0800

    差子密钥的生成算法了 还有每一轮的与子密钥的xor

--- DES/des.py
+++ DES/des.py
@@ -24,10 +24,11 @@ def binary_to_char(binary_sequence):
 
 #秘钥转换成01流
 def key_to_binary():
-    while True:
-        key = input("秘钥是什么: ")
-        if len(key)== data.len_key:
-            break
+    # while True:
+    #     key = input("秘钥是什么: ")
+    #     if len(key)== data.len_key:
+    #         break
+    key = 'aaaaaaaa'
     str = ''
     for x in key:
         str += char_to_binary(x)
@@ -45,4 +46,89 @@ def binary_to_plaintext(binary_sequence):
             str += binary_to_char(binary)
         result_string = ''.join(str)
     return result_string
+
+# IP置换
+def IP_change(bits):
+    '''
+    bits:一组64位的01比特字符串
+    return：初始置换IP后64bit01序列
+    '''
+    ip_str = ""
+    for i in data.IP:
+        ip_str = ip_str + bits[i - 1]
+    return ip_str
+
+# 扩散置换函数 32->48
+def E_change(bits):
+    '''
+    bits : 32bit01序列字符串
+    return : 扩展置换E后的48bit01字符串
+    '''
+    e = ""
+    for i in data.E:
+        e = e + bits[i-1]
+    return e
+
+# S盒代替函数48->32
+def S_change(bits):
+    '''
+    bits : 异或后的48bit01字符串
+    return : 经过S盒之后32bit01字符串
+    '''
+    P_S32 = ''
+    for i in range(8):
+        temp = bits[i*6:(i+1)*6]
+        row = int(temp[0] + temp[5], 2)
+        col = int(temp[1:5], 2)
+        num = bin(data.S_boxes[i][row * 16 + col])[2:]
+        num = num.zfill(4)
+        P_S32 = P_S32 + num
+    return P_S32
+
+# P置换
+def P_change(bits):
+    '''
+    bits:一组64位的01比特字符串
+    return：初始置换IP后64bit01序列
+    '''
+    ip_str = ""
+    for i in data.P:
+        # print(bits[i-1],i,sep = '-',end=' ')
+        ip_str = ip_str + bits[i - 1]
+    return ip_str
+
+#两个xor
+def xor(bits,ki):
+    '''
+    bits : 48bit01字符串 / 32bit01 F函数输出
+    ki : 48bit01密钥序列 / 32bit01 Li
+    return ：bits与ki异或运算得到的48bit01 / 32bit01
+    '''
+    bits_xor = ""
+    for i in range(len(bits)):
+       if bits[i] == ki[i]:
+           bits_xor += '0'
+       else:
+           bits_xor += '1'
+    return bits_xor
+
+#秘钥64->56
+def process_key(key):
+    key2 = ''
+    for i in range(len(key)):
+        if (i+1)%8==0:
+            continue
+        else:
+            key2 += key[i]
+    return key2
+
+
+def PC_1_change(bits):
+    ip_str = ""
+    for i in data.PC1:
+        # print(bits[i-1],i,sep = '-',end=' ')
+        ip_str = ip_str + bits[i - 1]
+    return ip_str
+
+
 
--- DES/main.py
+++ DES/main.py
@@ -4,11 +4,46 @@ import des
 
 if __name__ =="__main__":
 
-    plaintext = input("加密的文本内容是: ")
+    # plaintext = input("加密的文本内容是: ")
 
+    plaintext = "0110000101100001011000010110000101100001011000010110000101100001"#64位
     #字符串转01的比特流 返回的key是秘钥
     key = des.key_to_binary()
+    # key2 = des.process_key(key)
+    # print(key,key2,sep='\n')
+    # print(len(key2))
+    #秘钥置换选择1
+    key2 = des.PC_1_change(key)
+    print(f'秘钥key2',key2,len(key2))
+
+    #IP置换
+    plaintext_change_ip = des.IP_change(plaintext)
+    print('IP置换',plaintext_change_ip,len(plaintext_change_ip))
+    #分成左右两部分 右侧移到下一论的左边 左侧进行F函数
+    P_left32_i = plaintext_change_ip[0:32]
+    P_right32_i = plaintext_change_ip[32:]
+
+    print('左秘钥 右秘钥',P_left32_i,P_right32_i)
+
+    P_right48 = des.E_change(P_right32_i)
+    print('秘钥扩展',P_right48,len(P_right48))
+
+    #少了一个xor
+
+
+    P_S32 = des.S_change(P_right48)
+    print('S_boxs',P_S32,len(P_S32))
+
+    #置换P
+    P_S32 = des.P_change(P_S32)
+    print('P置换',P_S32, len(P_S32))
+
+    # print('',len(P_S32),len(P_left32_i))
+
+    P_right32_i_1 = des.xor(P_S32, P_left32_i)
+    print('xor异或2',P_right32_i_1,len(P_right32_i_1))
+
 
     #01比特流转字符串
     plaintext = des.binary_to_plaintext(key)
-    print(key,plaintext)
+    # print(key,plaintext)
